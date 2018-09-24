871a77b7ee298e0d208153752916830edf6cdb86
Nguyen Duc Viet <Ndviet2307@gmail.com>
2018-09-24 19:37:01 +0700

    Add admin template

---
+++ app/assets/javascripts/admin.js
@@ -0,0 +1,4 @@
+//= require jquery3
+//= require popper
+//= require jquery_ujs
+//= require bootstrap_sb_admin_base_v2
--- config/application.rb
+++ config/application.rb
@@ -28,7 +28,7 @@ module HotelBooking01
     # the framework and any gems in your application.
 
     # Don"t generate system test files.
-    config.generators.system_tests = nill
+    config.generators.system_tests = nil
     config.i18n.load_path += Dir[Rails.root.join("config", "locales", "**", "*.{rb,yml}")]
     config.i18n.available_locales = [:en, :vi]
     config.i18n.default_locale = :en
