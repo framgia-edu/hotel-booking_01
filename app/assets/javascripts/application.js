254c25abede6f075ae97cccca8f22075584650fe
Ly Van Minh <lyvanminh1002@gmail.com>
2018-09-28 14:14:27 +0700

    datetime_piker

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -1,7 +1,8 @@
 //= require jquery3
+//= require jquery.datetimepicker
 //= require popper
 //= require bootstrap-sprockets
 //= require rails-ujs
 //= require activestorage
+//= require event
 //= require turbolinks
-//= require_tree .
