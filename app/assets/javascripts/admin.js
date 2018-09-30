65df5c281d6d4f37c96dd3972d0cea80bfae530b
Nguyen Duc Viet <Ndviet2307@gmail.com>
2018-09-30 21:34:40 +0700

    Crud admin/posts part 2

--- app/assets/javascripts/admin.js
+++ app/assets/javascripts/admin.js
@@ -4,5 +4,7 @@
 //= require bootstrap_sb_admin_base_v2
 //= require admin/hotel
 //= require admin/category
+//= require admin/post
+//= require ckeditor/init
 //= require activestorage
 //= require turbolinks
